7fd94914b48bfbe2367eaa57ded0115dd4b67628
zyfmeng <2531407803@qq.com>
2016-08-17 16:32:14 +0800

    启动页
    启动页

---
+++ HelloLanch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloLanch
+//
+//  Created by md on 16/8/17.
+//  Copyright © 2016年 HKQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HelloLanch/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HelloLanch
+//
+//  Created by md on 16/8/17.
+//  Copyright © 2016年 HKQ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
